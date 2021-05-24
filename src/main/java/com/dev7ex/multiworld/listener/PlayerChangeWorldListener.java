1cc33f8d47f6ec70600f70a1d5dc4751ac227cbd
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2021-05-25 00:03:35 +0200

    adding "auto gamemode" feature

---
+++ src/main/java/com/dev7ex/multiworld/listener/PlayerChangeWorldListener.java
@@ -0,0 +1,34 @@
+package com.dev7ex.multiworld.listener;
+
+import com.dev7ex.multiworld.MultiWorldPlugin;
+import com.dev7ex.multiworld.event.MultiWorldListener;
+
+import com.dev7ex.multiworld.world.WorldProperties;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+
+/**
+ * @author Dev7ex
+ * @since 24.05.2021
+ */
+
+public final class PlayerChangeWorldListener extends MultiWorldListener {
+
+    public PlayerChangeWorldListener(final MultiWorldPlugin multiWorldPlugin) {
+        super(multiWorldPlugin);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public final void handlePlayerChangeWorld(final PlayerChangedWorldEvent event) {
+        final Player player = event.getPlayer();
+        final WorldProperties worldProperties = super.getWorldManager().getWorldProperties().get(player.getWorld().getName());
+
+        if (worldProperties == null) {
+            return;
+        }
+        player.setGameMode(worldProperties.getGameMode());
+    }
+
+}
