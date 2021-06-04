83532f775fee447f75e80352f004cd6fb32d60b3
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2021-06-04 19:38:10 +0200

    now in one class

--- src/main/java/com/dev7ex/multiworld/listener/PlayerConnectionListener.java
+++ src/main/java/com/dev7ex/multiworld/listener/PlayerConnectionListener.java
@@ -2,22 +2,26 @@ package com.dev7ex.multiworld.listener;
 
 import com.dev7ex.multiworld.MultiWorldPlugin;
 import com.dev7ex.multiworld.event.MultiWorldListener;
+import com.dev7ex.multiworld.user.WorldUser;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
- *
  * @author Dev7ex
- * @since 20.05.2021
- *
+ * @since 19.05.2021
  */
+public final class PlayerConnectionListener extends MultiWorldListener {
 
-public final class PlayerQuitListener extends MultiWorldListener {
-
-    public PlayerQuitListener(final MultiWorldPlugin multiWorldPlugin) {
+    public PlayerConnectionListener(final MultiWorldPlugin multiWorldPlugin) {
         super(multiWorldPlugin);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public final void handlePlayerLogin(final PlayerLoginEvent event) {
+        super.getWorldUserService().registerUser(new WorldUser(event.getPlayer().getUniqueId()));
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
