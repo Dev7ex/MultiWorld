947828d4fb8e0bbcbd5fc42b6e22b6bfd42186df
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2023-01-30 16:54:23 +0100

    Update MultiWorldConfiguration.java

--- src/main/java/com/dev7ex/multiworld/MultiWorldConfiguration.java
+++ src/main/java/com/dev7ex/multiworld/MultiWorldConfiguration.java
@@ -5,7 +5,9 @@ import com.dev7ex.common.bukkit.configuration.ConfigurationProperties;
 import com.dev7ex.common.bukkit.plugin.BukkitPlugin;
 import com.dev7ex.common.bukkit.plugin.configuration.DefaultPluginConfiguration;
 import com.dev7ex.common.map.ParsedMap;
+import com.dev7ex.multiworld.api.MultiWorldApiConfiguration;
 import com.dev7ex.multiworld.world.WorldOption;
+import lombok.SneakyThrows;
 
 import java.util.List;
 
@@ -14,7 +16,7 @@ import java.util.List;
  * @since 19.05.2021
  */
 @ConfigurationProperties(fileName = "config.yml")
-public final class MultiWorldConfiguration extends DefaultPluginConfiguration {
+public final class MultiWorldConfiguration extends DefaultPluginConfiguration implements MultiWorldApiConfiguration {
 
     private final ParsedMap<WorldOption, Object> worldDefaults = new ParsedMap<>();
     private final String messagePath = "messages.";
@@ -23,19 +25,30 @@ public final class MultiWorldConfiguration extends DefaultPluginConfiguration {
         super(bukkitPlugin);
     }
 
-    @Override
+    @Override @SneakyThrows
     public void load() {
         super.load();
 
         super.getFileConfiguration().getConfigurationSection("defaults").getKeys(false).stream()
                 .filter(entry -> !entry.equalsIgnoreCase("world")) // Ignore this
                 .forEach(entry -> this.worldDefaults.put(WorldOption.valueOf(entry.replaceAll("-", "_").toUpperCase()), super.getFileConfiguration().get(entry)));
+
+        this.loadChanges();
     }
 
+    public void loadChanges() {
+        if (!super.getFileConfiguration().contains("config-version")) {
+            super.getFileConfiguration().set("config-version", "1.3.1-SNAPSHOT");
+            super.saveFile();
+        }
+    }
+
+    @Override
     public List<String> getAutoLoadableWorlds() {
         return super.getStringList("auto-load");
     }
 
+    @Override
     public String getDefaultWorldName() {
         return super.getFileConfiguration().getString("defaults.world");
     }
