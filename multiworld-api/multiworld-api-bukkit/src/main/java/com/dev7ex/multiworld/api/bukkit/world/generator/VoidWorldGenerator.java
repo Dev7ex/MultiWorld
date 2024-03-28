e242e2f2509185a21289d0aedef2db628fe96c19
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-28 18:48:13 +0100

    Rename VoidChunkGenerator

--- multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/generator/VoidWorldGenerator.java
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/generator/VoidWorldGenerator.java
@@ -1,10 +1,12 @@
-package com.dev7ex.multiworld.api.bukkit.generator;
+package com.dev7ex.multiworld.api.bukkit.world.generator;
 
+import lombok.NoArgsConstructor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
@@ -13,11 +15,16 @@ import java.util.Random;
  * @author Dev7ex
  * @since 20.05.2021
  */
-public final class VoidChunkGenerator extends ChunkGenerator {
+@NoArgsConstructor
+public class VoidWorldGenerator extends BukkitWorldGenerator {
+
+    public VoidWorldGenerator(@NotNull final Plugin plugin) {
+        super(plugin);
+    }
 
     @Override
-    public @NotNull ChunkData generateChunkData(@NotNull final World world, @NotNull final Random random, final int x, final int z, @NotNull final BiomeGrid biome) {
-        final ChunkData chunkData = super.createChunkData(world);
+    public @NotNull ChunkGenerator.ChunkData generateChunkData(@NotNull final World world, @NotNull final Random random, final int x, final int z, @NotNull final ChunkGenerator.BiomeGrid biome) {
+        final ChunkGenerator.ChunkData chunkData = super.createChunkData(world);
 
         if ((x == 0) && (z == 0)) {
             chunkData.setBlock(0, 63, 0, Material.STONE);
