07a7e1a7d3849ca474262f95b942687e84d28344
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-03-28 18:48:45 +0100

    Rename FlatChunkGenerator

--- multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/generator/FlatWorldGenerator.java
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/generator/FlatWorldGenerator.java
@@ -1,9 +1,11 @@
-package com.dev7ex.multiworld.api.bukkit.generator;
+package com.dev7ex.multiworld.api.bukkit.world.generator;
 
+import lombok.NoArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
@@ -12,11 +14,16 @@ import java.util.Random;
  * @author Dev7ex
  * @since 20.05.2021
  */
-public final class FlatChunkGenerator extends ChunkGenerator {
+@NoArgsConstructor
+public class FlatWorldGenerator extends BukkitWorldGenerator {
+
+    public FlatWorldGenerator(@NotNull final Plugin plugin) {
+        super(plugin);
+    }
 
     @Override
-    public @NotNull ChunkData generateChunkData(@NotNull final World world, @NotNull final Random random, final int x, final int z, @NotNull final BiomeGrid biome) {
-        final ChunkData chunkData = super.createChunkData(world);
+    public @NotNull ChunkGenerator.ChunkData generateChunkData(@NotNull final World world, @NotNull final Random random, final int x, final int z, @NotNull final ChunkGenerator.BiomeGrid biome) {
+        final ChunkGenerator.ChunkData chunkData = super.createChunkData(world);
         chunkData.setRegion(0, 0, 0, 16, 1, 16, Material.BEDROCK);
         chunkData.setRegion(0, 1, 0, 16, 64, 16, Material.GRASS_BLOCK);
 
