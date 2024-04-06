408beeae14c7c0efa8e0179f63adcd43e4575e36
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-04-06 21:49:56 +0200

    Adding Configurable SimpleDateFormat

--- multiworld-bukkit/src/main/java/com/dev7ex/multiworld/command/world/InfoCommand.java
+++ multiworld-bukkit/src/main/java/com/dev7ex/multiworld/command/world/InfoCommand.java
@@ -4,13 +4,13 @@ import com.dev7ex.common.bukkit.command.BukkitCommand;
 import com.dev7ex.common.bukkit.command.BukkitCommandProperties;
 import com.dev7ex.common.bukkit.command.completer.BukkitTabCompleter;
 import com.dev7ex.common.bukkit.plugin.BukkitPlugin;
+import com.dev7ex.multiworld.MultiWorldConfiguration;
 import com.dev7ex.multiworld.MultiWorldPlugin;
 import com.dev7ex.multiworld.api.bukkit.world.BukkitWorldHolder;
 import com.dev7ex.multiworld.api.world.WorldEnvironment;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -45,12 +45,13 @@ public class InfoCommand extends BukkitCommand implements BukkitTabCompleter {
             return;
         }
         final BukkitWorldHolder worldHolder = MultiWorldPlugin.getInstance().getWorldProvider().getWorldHolder(arguments[1]).get();
+        final MultiWorldConfiguration configuration = MultiWorldPlugin.getInstance().getConfiguration();
 
-        super.getConfiguration().getStringList("messages.commands.info.message").forEach(message -> {
+        configuration.getStringList("messages.commands.info.message").forEach(message -> {
             commandSender.sendMessage(message
                     .replaceAll("%world_name%", worldHolder.getName())
                     .replaceAll("%world_creator_name%", worldHolder.getCreatorName())
-                    .replaceAll("%creation_timestamp%", new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date(worldHolder.getCreationTimeStamp())))
+                    .replaceAll("%creation_timestamp%", configuration.getTimeFormat().format(new Date(worldHolder.getCreationTimeStamp())))
                     .replaceAll("%load_auto%", String.valueOf(worldHolder.isAutoLoaded()))
                     .replaceAll("%loaded%", (worldHolder.isLoaded() ? "true" : "false"))
                     .replaceAll("%world_type%", worldHolder.getType().toString())
@@ -60,9 +61,14 @@ public class InfoCommand extends BukkitCommand implements BukkitTabCompleter {
                     .replaceAll("%pvp_enabled%", (worldHolder.isPvpEnabled() ? "true" : "false"))
                     .replaceAll("%spawn_animals%", (worldHolder.isSpawnAnimals() ? "true" : "false"))
                     .replaceAll("%spawn_monsters%", (worldHolder.isSpawnMonsters() ? "true" : "false"))
+                    .replaceAll("%spawn_entities%", (worldHolder.isSpawnEntities() ? "true" : "false"))
                     .replaceAll("%end-portal-accessible%", (worldHolder.isEndPortalAccessible() ? "true" : "false"))
                     .replaceAll("%nether-portal-accessible%", (worldHolder.isNetherPortalAccessible() ? "true" : "false"))
-                    .replaceAll("%whitelist_enabled%", (worldHolder.isWhitelistEnabled() ? "true" : "false")));
+                    .replaceAll("%whitelist_enabled%", (worldHolder.isWhitelistEnabled() ? "true" : "false"))
+                    .replaceAll("%normal_world%", worldHolder.getNormalWorldName() == null ? "" : worldHolder.getNormalWorldName())
+                    .replaceAll("%nether_world%", worldHolder.getNetherWorldName() == null ? "" : worldHolder.getNetherWorldName())
+                    .replaceAll("%end_world%", worldHolder.getEndWorldName() == null ? "" : worldHolder.getEndWorldName())
+                    .replaceAll("%receive_achievements%", (worldHolder.isReceiveAchievements() ? "true" : "false")));
         });
         return;
     }
