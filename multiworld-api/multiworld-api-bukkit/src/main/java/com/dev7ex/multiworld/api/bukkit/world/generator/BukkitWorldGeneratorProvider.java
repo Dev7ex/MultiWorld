503c7ca10f3e305355eeb90de2935fbc2c23957b
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-06-07 14:51:16 +0200

    Create BukkitWorldGeneratorProvider.java

---
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/generator/BukkitWorldGeneratorProvider.java
@@ -0,0 +1,13 @@
+package com.dev7ex.multiworld.api.bukkit.world.generator;
+
+import com.dev7ex.multiworld.api.world.generator.WorldGeneratorProvider;
+
+/**
+ * Represents a provider for Bukkit world generators in a multi-world environment.
+ * Extends WorldGeneratorProvider interface.
+ * This interface provides functionality for managing Bukkit world generators.
+ *
+ * @author Dev7ex
+ * @since 29.06.2023
+ */
+public interface BukkitWorldGeneratorProvider extends WorldGeneratorProvider<BukkitWorldGeneratorHolder> {}
