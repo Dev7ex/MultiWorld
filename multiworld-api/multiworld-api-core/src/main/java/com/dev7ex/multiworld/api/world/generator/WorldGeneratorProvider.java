78ef52ec64b826dc19c6949350c0858bd22ccc22
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-06-07 14:54:40 +0200

    Create WorldGeneratorProvider.java

---
+++ multiworld-api/multiworld-api-core/src/main/java/com/dev7ex/multiworld/api/world/generator/WorldGeneratorProvider.java
@@ -0,0 +1,42 @@
+package com.dev7ex.multiworld.api.world.generator;
+
+import java.util.Map;
+
+/**
+ * Represents a provider for world generators.
+ * Implementations of this interface provide methods to retrieve a map of world generators.
+ *
+ * @param <T> The type of WorldGeneratorHolder.
+ * @since 29.06.2023
+ */
+public interface WorldGeneratorProvider<T extends WorldGeneratorHolder> {
+
+    /**
+     * Retrieves a map of custom world generators.
+     *
+     * @return A map containing instances of WorldGeneratorHolder as keys and their corresponding world generator names as values.
+     */
+    Map<T, String> getCustomGenerators();
+
+    /**
+     * Retrieves a map of file-based world generators.
+     *
+     * @return A map containing instances of WorldGeneratorHolder as keys and their corresponding world generators as values.
+     */
+    Map<T, WorldGenerator> getFileGenerators();
+
+    /**
+     * Retrieves a map of default world generators.
+     *
+     * @return A map containing instances of WorldGeneratorHolder as keys and their corresponding default world generators as values.
+     */
+    Map<T, WorldGenerator> getDefaultGenerators();
+
+    /**
+     * Checks if a custom world generator is registered.
+     *
+     * @param generator The name of the generator to check.
+     * @return True if the generator is registered, false otherwise.
+     */
+    boolean isRegistered(String generator);
+}
