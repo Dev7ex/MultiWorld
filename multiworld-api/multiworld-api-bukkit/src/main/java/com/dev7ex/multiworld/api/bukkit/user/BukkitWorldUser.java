4ef31f472e75b908200a918a29f23843f20633bd
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-06-07 14:50:53 +0200

    Create BukkitWorldUser.java

---
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/user/BukkitWorldUser.java
@@ -0,0 +1,11 @@
+package com.dev7ex.multiworld.api.bukkit.user;
+
+import com.dev7ex.common.bukkit.entity.EntityResolver;
+import com.dev7ex.multiworld.api.user.WorldUser;
+import org.bukkit.entity.Player;
+
+/**
+ * @author Dev7ex
+ * @since 07.06.2024
+ */
+public interface BukkitWorldUser extends WorldUser, EntityResolver<Player> {}
