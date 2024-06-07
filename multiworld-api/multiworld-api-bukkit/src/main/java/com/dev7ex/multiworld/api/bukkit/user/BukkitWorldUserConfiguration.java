64d8f63224ce93503cb1bd8f2b6f50ad736bd705
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-06-07 14:54:31 +0200

    Create BukkitWorldUserConfiguration.java

---
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/user/BukkitWorldUserConfiguration.java
@@ -0,0 +1,9 @@
+package com.dev7ex.multiworld.api.bukkit.user;
+
+import com.dev7ex.multiworld.api.user.WorldUserConfiguration;
+
+/**
+ * @author Dev7ex
+ * @since 07.06.2024
+ */
+public interface BukkitWorldUserConfiguration extends WorldUserConfiguration {}
