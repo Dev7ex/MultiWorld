c73a18a8d6c1d6e69de6f034e19b3887ced07580
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-06-12 00:11:35 +0200

    Update BukkitWorldProvider.java

--- multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/BukkitWorldProvider.java
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/BukkitWorldProvider.java
@@ -10,4 +10,5 @@ import com.dev7ex.multiworld.api.world.WorldProvider;
  * @author Dev7ex
  * @since 18.06.2023
  */
-public interface BukkitWorldProvider extends WorldProvider<BukkitWorldHolder> {}
+public interface BukkitWorldProvider extends WorldProvider<BukkitWorldHolder> {
+}
