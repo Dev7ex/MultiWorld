365a6c093650d646014c7ff7ca298b498cfa748c
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-06-12 00:11:40 +0200

    Update BukkitWorldUserConfiguration.java

--- multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/user/BukkitWorldUserConfiguration.java
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/user/BukkitWorldUserConfiguration.java
@@ -6,4 +6,5 @@ import com.dev7ex.multiworld.api.user.WorldUserConfiguration;
  * @author Dev7ex
  * @since 07.06.2024
  */
-public interface BukkitWorldUserConfiguration extends WorldUserConfiguration {}
+public interface BukkitWorldUserConfiguration extends WorldUserConfiguration {
+}
