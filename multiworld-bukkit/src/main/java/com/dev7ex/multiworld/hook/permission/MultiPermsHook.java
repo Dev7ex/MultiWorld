4c11976340012bed3dbb903c9023675a0ad5b342
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-08-19 01:47:36 +0200

    Create MultiPerms Hook

---
+++ multiworld-bukkit/src/main/java/com/dev7ex/multiworld/hook/permission/MultiPermsHook.java
@@ -0,0 +1,47 @@
+package com.dev7ex.multiworld.hook.permission;
+
+import com.dev7ex.multiperms.api.MultiPermsApiProvider;
+import com.dev7ex.multiperms.api.hook.PermissionHook;
+import com.dev7ex.multiworld.MultiWorldPlugin;
+
+import java.util.List;
+
+/**
+ * @author Dev7ex
+ * @since 19.08.2024
+ */
+public class MultiPermsHook implements PermissionHook {
+
+    @Override
+    public List<String> getPermissions() {
+        return List.of("multiworld.command.world",
+                "multiworld.command.world.back",
+                "multiworld.command.world.backup",
+                "multiworld.command.world.clone",
+                "multiworld.command.world.create",
+                "multiworld.command.world.delete",
+                "multiworld.command.world.flag",
+                "multiworld.command.world.gamerule",
+                "multiworld.command.world.help",
+                "multiworld.command.world.import",
+                "multiworld.command.world.info",
+                "multiworld.command.world.link",
+                "multiworld.command.world.list",
+                "multiworld.command.world.load",
+                "multiworld.command.world.reload",
+                "multiworld.command.world.teleport",
+                "multiworld.command.world.unload",
+                "multiworld.command.world.version",
+                "multiworld.command.world.whitelist",
+                "multiworld.command.world.whitelist.add",
+                "multiworld.command.world.whitelist.disable",
+                "multiworld.command.world.whitelist.enable",
+                "multiworld.command.world.whitelist.list",
+                "multiworld.command.world.whitelist.remove");
+    }
+
+    public void register() {
+        MultiPermsApiProvider.getMultiPermsApi().getPermissionHookProvider().register(MultiWorldPlugin.getInstance(), this);
+    }
+
+}
