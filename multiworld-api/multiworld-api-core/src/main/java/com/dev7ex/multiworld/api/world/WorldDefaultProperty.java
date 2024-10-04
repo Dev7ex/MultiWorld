fc10bfe3b67d121b03b538af485e7cf7378c2a40
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-10-05 00:29:12 +0200

    Update WorldDefaultProperty.java

--- multiworld-api/multiworld-api-core/src/main/java/com/dev7ex/multiworld/api/world/WorldDefaultProperty.java
+++ multiworld-api/multiworld-api-core/src/main/java/com/dev7ex/multiworld/api/world/WorldDefaultProperty.java
@@ -21,13 +21,13 @@ public enum WorldDefaultProperty {
     AUTO_UNLOAD_ENABLED("auto-unload-enabled", "settings.defaults.auto-unload-enabled"),
     DIFFICULTY("difficulty", "settings.defaults.difficulty"),
     END_PORTAL_ACCESSIBLE("end-portal-accessible", "settings.defaults.end-portal-accessible"),
+    END_WORLD("end-world", "settings.defaults.end-world"),
     GAME_MODE("game-mode", "settings.defaults.game-mode"),
     HUNGER_ENABLED("hunger-enabled", "settings.defaults.hunger-enabled"),
     KEEP_SPAWN_IN_MEMORY("keep-spawn-in-memory", "settings.defaults.keep-spawn-in-memory"),
-    END_WORLD("end-world", "settings.defaults.end-world"),
+    NETHER_PORTAL_ACCESSIBLE("nether-portal-accessible", "settings.defaults.nether-portal-accessible"),
     NETHER_WORLD("nether-world", "settings.defaults.nether-world"),
     NORMAL_WORLD("world", "settings.defaults.normal-world"),
-    NETHER_PORTAL_ACCESSIBLE("nether-portal-accessible", "settings.defaults.nether-portal-accessible"),
     PVP_ENABLED("pvp-enabled", "settings.defaults.pvp-enabled"),
     RECEIVE_ACHIEVEMENTS("receive-achievements", "settings.defaults.receive-achievements"),
     REDSTONE_ENABLED("redstone-enabled", "settings.defaults.redstone-enabled"),
@@ -37,6 +37,7 @@ public enum WorldDefaultProperty {
     WEATHER_ENABLED("weather-enabled", "settings.defaults.weather-enabled"),
     WHITELIST_ENABLED("whitelist-enabled", "settings.defaults.whitelist-enabled"),
     WORLD("world", "settings.defaults.normal-world");
+
 
 
     private final String name;
