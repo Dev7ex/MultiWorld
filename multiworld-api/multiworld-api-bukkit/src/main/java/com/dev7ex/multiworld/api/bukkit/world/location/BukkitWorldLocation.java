901958905379dd2dc89a489d00bdbfcafc948051
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-10-10 17:03:29 +0200

    Update BukkitWorldLocation.java

--- multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/location/BukkitWorldLocation.java
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/location/BukkitWorldLocation.java
@@ -4,12 +4,7 @@ import com.dev7ex.multiworld.api.world.location.WorldLocation;
 import lombok.*;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.configuration.serialization.ConfigurationSerializable;
-import org.bukkit.configuration.serialization.SerializableAs;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Dev7ex
@@ -19,8 +14,7 @@ import java.util.Map;
 @Getter(AccessLevel.PUBLIC)
 @Builder(setterPrefix = "set")
 @NoArgsConstructor
-@SerializableAs("BukkitWorldLocation")
-public class BukkitWorldLocation implements WorldLocation, ConfigurationSerializable {
+public class BukkitWorldLocation implements WorldLocation {
 
     private String worldName;
     private double x;
@@ -38,29 +32,6 @@ public class BukkitWorldLocation implements WorldLocation, ConfigurationSerializ
         this.pitch = pitch;
     }
 
-    private BukkitWorldLocation(@NotNull final Map<String, Object> serializeData) {
-        this.worldName = (String) serializeData.get("world-name");
-        this.x = (double) serializeData.get("x");
-        this.y = (double) serializeData.get("y");
-        this.z = (double) serializeData.get("z");
-
-        if (serializeData.containsKey("yaw")) {
-            this.yaw = (double) serializeData.get("yaw");
-        }
-
-        if (serializeData.containsKey("pitch")) {
-            this.pitch = (double) serializeData.get("pitch");
-        }
-    }
-
-    public static BukkitWorldLocation deserialize(@NotNull final Map<String, Object> serializeData) {
-        return new BukkitWorldLocation(serializeData);
-    }
-
-    public static BukkitWorldLocation valueOf(@NotNull final Map<String, Object> serializeData) {
-        return new BukkitWorldLocation(serializeData);
-    }
-
     public static BukkitWorldLocation of(@NotNull final Location location) {
         final BukkitWorldLocation worldLocation = new BukkitWorldLocation();
         worldLocation.setWorldName(location.getWorld().getName());
@@ -74,19 +45,6 @@ public class BukkitWorldLocation implements WorldLocation, ConfigurationSerializ
 
     public static Location to(@NotNull final WorldLocation location) {
         return new Location(Bukkit.getWorld(location.getWorldName()), location.getX(), location.getY(), location.getZ(), (float) location.getYaw(), (float) location.getPitch());
-    }
-
-    @Override
-    public Map<String, Object> serialize() {
-        final Map<String, Object> serializeData = new HashMap<>();
-
-        serializeData.put("world-name", this.worldName);
-        serializeData.put("x", this.x);
-        serializeData.put("y", this.y);
-        serializeData.put("z", this.z);
-        serializeData.put("yaw", this.yaw);
-        serializeData.put("pitch", this.pitch);
-        return serializeData;
     }
 
 }
