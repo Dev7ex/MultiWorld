9d9b59d42ab212b762e80a0aa1f7f64ed4c1742a
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-10-27 17:15:19 +0100

    Create Colored.java

---
+++ multiworld-bukkit/src/main/java/com/dev7ex/multiworld/util/Colored.java
@@ -0,0 +1,39 @@
+package com.dev7ex.multiworld.util;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Difficulty;
+import org.bukkit.GameMode;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Dev7ex
+ * @since 22.10.2024
+ */
+public class Colored {
+
+    public static String getColoredBoolean(final boolean value) {
+        if (value) {
+            return ChatColor.GREEN + "true";
+        }
+        return ChatColor.RED + "false";
+    }
+
+    public static String getColoredGameMode(@NotNull final GameMode gameMode) {
+        return switch (gameMode) {
+            case ADVENTURE, SURVIVAL -> ChatColor.GREEN + gameMode.toString();
+            case CREATIVE -> ChatColor.RED + gameMode.toString();
+            case SPECTATOR -> ChatColor.DARK_GREEN + gameMode.toString();
+        };
+    }
+
+    public static String getColoredDifficulty(@NotNull final Difficulty difficulty) {
+        return switch (difficulty) {
+            case PEACEFUL -> ChatColor.GREEN + difficulty.toString();
+            case EASY -> ChatColor.DARK_GREEN + difficulty.toString();
+            case NORMAL -> ChatColor.YELLOW + difficulty.toString();
+            case HARD -> ChatColor.RED + difficulty.toString();
+        };
+    }
+
+}
