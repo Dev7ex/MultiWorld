125418ac3bc83c15c1edf089f5e23dc76d9cd208
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-12-07 12:53:07 +0100

    Update BiomeHolder.java

--- multiworld-api/multiworld-api-core/src/main/java/com/dev7ex/multiworld/api/world/biome/BiomeHolder.java
+++ multiworld-api/multiworld-api-core/src/main/java/com/dev7ex/multiworld/api/world/biome/BiomeHolder.java
@@ -4,4 +4,8 @@ package com.dev7ex.multiworld.api.world.biome;
  * @author Dev7ex
  * @since 23.02.2024
  */
-public interface BiomeHolder {}
+public interface BiomeHolder<V> {
+
+    V getBiome();
+
+}
