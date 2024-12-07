9abe702c049377f5b62ddca6582481d6b4a0e97e
Dev7ex <46531389+Dev7ex@users.noreply.github.com>
2024-12-07 12:53:10 +0100

    Create BukkitBiomeHolder.java

---
+++ multiworld-api/multiworld-api-bukkit/src/main/java/com/dev7ex/multiworld/api/bukkit/world/biome/BukkitBiomeHolder.java
@@ -0,0 +1,11 @@
+package com.dev7ex.multiworld.api.bukkit.world.biome;
+
+import com.dev7ex.multiworld.api.world.biome.BiomeHolder;
+import org.bukkit.block.Biome;
+
+/**
+ * @author Dev7ex
+ * @since 06.11.2024
+ */
+public interface BukkitBiomeHolder extends BiomeHolder<Biome> {
+}
